c0d5f78a00310beae42e99a0ede7d450a7f4d78c
Rose Marie Philip <rosephilip@rosephilip-mbp.local.dhcp.thefacebook.com>
2015-02-21 18:19:07 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Rose Marie Philip on 2/21/15.
+//  Copyright (c) 2015 Rose Marie Philip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Rose Marie Philip on 2/21/15.
+//  Copyright (c) 2015 Rose Marie Philip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
